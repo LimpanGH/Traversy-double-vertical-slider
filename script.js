2315354ac2ab289d0f01d3883841e5579bcfbaad
LimpanGH <Linusj555@gmail.com>
2023-03-12 06:33:39 +0100

    added to css and js

---
+++ script.js
@@ -0,0 +1,28 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide ')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+// console.log(slidesLength); Gives us 4 in the console.
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight       // console.log(sliderHeight)  Gives us the current height in pixels in the console.
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(ac)
+    }
+
+}
+
+
+
+
