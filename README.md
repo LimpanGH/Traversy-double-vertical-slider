6ca600e299eb8afd9b124315179b344740d1896f
LimpanGH <Linusj555@gmail.com>
2023-03-15 11:34:45 +0100

    added more comments to .css

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ https://www.youtube.com/watch?v=JkeyKeK3V24 Timestamp 15:31
 https://github.com/bradtraversy/50projects50days/tree/master/double-vertical-slider 
 
 
-Questions:
-___________________________________________
+
+Questions: ____________________________________________________________
+
+CSS - - - - - - -
 
 * .css line 22.  Why do we set "overflow: hidden;", theres not really an abundance of text.
 
@@ -26,7 +28,21 @@ button:focus {
     outline: none;
 }
 
-* 
+
+Java Script - - - - - - - -
+
+* .js line 14: Fattar inte vad raden innebär, förutom att den medverkar till att se till att rätt bild hör till rätt text.
+  slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+* .js line 19: Should'nt this affect all slides from 0-3??
+  const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength -1) {
+            activeSlideIndex = 0
+
+* .js line 37: I don't understand how "slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`" is connecting to ".clientHeight" on line 20. Timestamp 38:30
 
 
 
--- script.js
+++ script.js
@@ -1,35 +1,39 @@
+
+// These calls all the classes in our html-document that we need.
 const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide ')
 const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+const slidesLength = slideRight.querySelectorAll('div').length  // Checks how many slides we have. This one calls all the divs in slideRight our html document. So line 8 calls line 4 which calls .right-slide in our html. 
 
 // console.log(slidesLength); Gives us 4 in the console.
 
-let activeSlideIndex = 0
+let activeSlideIndex =0                                         // The pictures are positioned on top of eachother so we need to know which index is in view.                       
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`          
 
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
+upButton.addEventListener('click', () => changeSlide('up'))     // When we click the up-button, we run a function that we can name "changeSlide" and we pass in an argument of 'up'.  
+downButton.addEventListener('click', () => changeSlide('down')) // When we click the down-button, we run a function that we can name "changeSlide" and we pass in an argument of 'down'.  
 
-const changeSlide = (direction) => {
-    const sliderHeight = sliderContainer.clientHeight       // console.log(sliderHeight)  Gives us the current height in pixels in the console.
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex > slidesLength -1) {
-            activeSlideIndex = 0
+const changeSlide = (direction) => {                            // Creating our function that we name "changeSlide", it takes in direction as a parameter. 
+    const sliderHeight = sliderContainer.clientHeight           // Creating "sliderHeight" and set it to be sliderContainer and add the property clientHeight. As I understand it, this adapts the picture height to the current vieport in our browser??? console.log(sliderHeight)  Gives us the current height in pixels in the console.
+    if(direction === 'up') {                                    // Checking for the direction that is passed in. If direction equals to 'up'...
+        activeSlideIndex++                                      // then take "activeSlideIndex" and increment it by 1.
+        if(activeSlideIndex > slidesLength -1) {                // and when we hit the end i.e if "activeSlideIndex" is greater than the last index in the slide. First picture has the index of 0, and if the active index is greater than -1...
+            activeSlideIndex = 0                                // then we reset the slide to 0, i.e the beginning.                            
         }
     } else if(direction === 'down') {
         activeSlideIndex--
         if(activeSlideIndex < 0) {
             activeSlideIndex = slidesLength -1
         }
-    }
+    } 
 
-slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+/* This code allows the photos to actually slide up or down.
+   As I understand it, the .style property returns the inline style of the HTML element in question, i.e "background-image". 
+   The .transform CSS property lets you rotate, scale, skew, or translate an element. It modifies the coordinate space of the CSS visual formatting model, https://developer.mozilla.org/en-US/docs/Web/CSS/transform.   
+    */
+slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`  // Using a Template String to translate the picture in our "slideRight" upwards, hence the minus-sign before the $. We calculate how much it should slide by multiplying "activeSlideIndex" by "sliderHeight" and output it in amount of pixels.
+slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`    // Using a Template String to translate the picture in our "slideRight" downwards.
 }
-
-// https://www.youtube.com/watch?v=JkeyKeK3V24 36.32
